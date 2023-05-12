a90c772b9184d9aa73555ea78d78afa24ecdc3b0
Ciprian Cretu <124049021+cipriancretu@users.noreply.github.com>
2023-05-12 17:44:07 +0100

    Update player.py

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+"""Import turtle library"""
 from turtle import Turtle
 
 STARTING_POSITION = (0, -280)
@@ -13,13 +14,13 @@ class Player(Turtle):
         self.penup()
         self.go_to_start()
         self.setheading(90)
-
+    """function set up the movment for the turtle"""
     def go_up(self):
         self.forward(MOVE_DISTANCE)
-
+    """function set up the starting position of the turtle"""
     def go_to_start(self):
         self.goto(STARTING_POSITION)
-
+    """"""function determin if the turtle has reach the finish line"""
     def is_at_finish_line(self):
         if self.ycor() > FINISH_LINE_Y:
             return True
