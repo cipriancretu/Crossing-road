15fab90c2e8e34afd4401361bc864db19a2a7bd9
Ciprian Cretu <124049021+cipriancretu@users.noreply.github.com>
2023-05-12 16:10:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+"""Import the modules and classes"""
 import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+"""Set up the screen"""
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
